cdbfb8b16843b8079e57c43a95f1d187d2fc4639
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-06-29 09:20:42 +0200

    add defer

--- 21-ReactRouterProject/frontend/src/pages/Events.js
+++ 21-ReactRouterProject/frontend/src/pages/Events.js
@@ -1,24 +1,39 @@
-import { Link } from "react-router-dom";
+import { Suspense } from "react";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
 
-const EVENTS = [
-  { id: "e1", title: "Event 1" },
-  { id: "e2", title: "Event 2" },
-  { id: "e3", title: "Event 3" },
-];
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
+  const { events } = useLoaderData();
+
   return (
-    <>
-      <h1>Events Page</h1>
-      <ul>
-        {EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isError: true, message: "Could not fetch events" };
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    // status: 500,
+    // });
+    throw json({ message: "Could not fetch events." }, { status: 500 }); // This is the same as the above
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
+}
