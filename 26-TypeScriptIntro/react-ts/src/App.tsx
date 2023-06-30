9430a21a63c974267193a8e29589777378265ae1
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-06-30 16:13:35 +0200

    typescript removing todo

--- 26-TypeScriptIntro/react-ts/src/App.tsx
+++ 26-TypeScriptIntro/react-ts/src/App.tsx
@@ -1,12 +1,30 @@
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 import "./App.css";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  const [todos, setTodos] = useState<Todo[]>([]); // [Todo, Todo, Todo
+
+  // const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div>
-      <Todos items={todos} />
+      <NewTodo />
+      <Todos />
     </div>
   );
 }
